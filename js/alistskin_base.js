afed06bb31789dd3a9089c279ca9702280dc376d
root <root@localhost>
2023-03-26 06:43:37 +0800

    new config

---
+++ js/alistskin_base.js
@@ -0,0 +1,12 @@
+/**
+ * @name alistskin Alist云盘美化程序
+ * @edition 2.0.3
+ * @author flower
+ * @mtime 2022-10-19
+ * @message 前端美化样式由星环[Flower]提供，程序由Alist创始者友情提供
+ * @community http://alistskin.xhto.cn/
+ */
+
+var x = true;
+var conarr = ["启明星-云盘美化包","https://alistskin.xhto.cn/"];
+console.log('%c '+conarr[0]+' %c '+conarr[1]+' ','padding: 5px 10px; color: #FFF; background-color:#282828; border-radius: 2px 0px 0px 2px;','padding: 5px 10px;color: #FFF; background-image: linear-gradient(to left,#00f2fe,#66ccff); border-radius: 0px 2px 2px 0px;');
---
+++ js/alistskin_load.js
@@ -0,0 +1,14 @@
+// 个人信息编写区域+星环博客提供技术支持+ —————— END
+var code = "";
+code += '<h3>'+title+'</h3>';code += '<div id="uxhdimg"><img src="'+headimg+'" alt="['+title+']" /></div>';code += '<p>'+motto+'</p>';code += '<ul>';
+for (vo in linkname) { code += '<li><a href="'+link[vo]+'">'+linkname[vo]+'</a></li>'; } code += '</ul>';
+$(".header").append(code);
+x = false;
+}})
+$(document).on('click', '.buttons>span:eq(1) svg', function() { var $viwe = $(this).attr('viewBox'); })
+window.onload = function(){ setTimeout(function() { if($("#uxhdimg").length <= 0){ x = true; } if(x){ $("html").click(); x = false; } }, 200);
+setTimeout(function() { if($("#uxhdimg").length <= 0){ x = true; } if(x){ $("html").click(); x = false; } }, 500);
+setTimeout(function() { if($("#uxhdimg").length <= 0){ x = true; } if(x){ $("html").click(); x = false; } }, 1000);
+setTimeout(function() { if($("#uxhdimg").length <= 0){ x = true; } if(x){ $("html").click(); x = false; } }, 2000);
+setTimeout(function() { if($("#uxhdimg").length <= 0){ x = true; } if(x){ $("html").click(); x = false; } }, 5000);
+}
